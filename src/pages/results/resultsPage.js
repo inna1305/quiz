294e2cf0b6b5366b3b824a0833b17ec40fe8181a
Inna T <meeoow005@gmail.com>
2023-05-30 00:46:14 +0200

    feat: link button to return to the form page

--- src/pages/results/resultsPage.js
+++ src/pages/results/resultsPage.js
@@ -1,9 +1,17 @@
 import createElement from "../../helpers/createElement";
 import getResultElement from "./getResultElement";
+import clearElement from "../../helpers/clearElement";
+import mainPage from "../main/mainPage";
 
 const resultsPage = (data) => {
     const container = createElement('div', { class: 'container-results' });
     const button = createElement('button', { class: 'button' }, "Пройти тест заново");
+    button.addEventListener('click', () => {
+        const body = document.querySelector('body');
+        clearElement(body);
+        body.append(mainPage());
+    })
+
     container.append(button);
     const resultsContainer = createElement('div', { class: 'institutes' });
     data.institutions.forEach(obj => {
